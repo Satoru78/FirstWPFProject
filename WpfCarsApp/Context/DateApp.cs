894943a1a54844ba19577d907f062cb460a4f36b
Satoru78 <viewgold931@gmail.com>
2022-01-02 18:04:55 +0300

    Добавьте файлы проекта.

---
+++ WpfCarsApp/Context/DateApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfCarsApp.Model;
+
+namespace WpfCarsApp.Context
+{
+     internal static class DateApp
+    {
+        public static CarsEntities ce = new CarsEntities();
+    }
+}
---
+++ WpfCarsApp/Views/Pages/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfCarsApp.Context;
+
+namespace WpfCarsApp.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataView.ItemsSource = DateApp.ce.Cars.ToList(); 
+        }
+
+        private void Search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            DataView.ItemsSource = DateApp.ce.Cars.Where(item => item.Brand.Contains(Search.Text) || item.YearIssue.Contains(Search.Text) ||
+            item.Mileage.ToString().Contains(Search.Text) || item.Price.ToString().Contains(Search.Text)).ToList();
+        }
+    }
+}
